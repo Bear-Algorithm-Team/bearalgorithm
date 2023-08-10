e48131a135cacae159ca35c486c91e3c1245655c
Kangwook Lee <kangwooklee29@gmail.com>
2023-08-11 01:14:29 +0900

    Initial commit for this branch

--- geekbot/setup_geekbot.sh
+++ geekbot/setup_geekbot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # check if coreutils is installed
-if ! command -v coreutils &> /dev/null; then
+if ! command -v gls &> /dev/null; then
     echo "Error: coreutils is not installed."
     echo "Please install coreutils using 'brew install coreutils' and try again."
     exit 1
@@ -14,8 +14,16 @@ if ! command -v jq &> /dev/null; then
     exit 1
 fi
 
+# Check if the 'discord' module is installed in /usr/bin/python3
+if ! /usr/bin/python3 -c "import discord" 2>/dev/null; then
+    echo "Error: The 'discord' module is not installed for /usr/bin/python3."
+    echo "Please install it using the command: /usr/bin/pip3 install discord.py"
+    exit 1
+fi
+
 # Get values from config.json
-CONFIG_FILE="./config.json"
+SCRIPT_PATH="$(cd "$(dirname "$0")"; pwd -P)"
+CONFIG_FILE="${SCRIPT_PATH}/config.json"
 CRONTAB=$(jq -r '.CRONTAB' $CONFIG_FILE)
 GUILD_ID=$(jq -r '.GUILD_ID' $CONFIG_FILE)
 CHANNEL_NAME=$(jq -r '.CHANNEL_NAME' $CONFIG_FILE)
@@ -25,13 +33,16 @@ GEEKBOT_ID=$(jq -r '.GEEKBOT_ID' $CONFIG_FILE)
 crontab -l > /tmp/cron_temp
 
 # Loop through each user in the USERS array
+TODAY=$(date +"%d-%m-%Y")
 length=$(jq '.USERS | length' $CONFIG_FILE)
 for (( i = 0 ; i < $length ; i++ )); do
     USER_ID=$(jq -r ".USERS[$i].USER_ID" $CONFIG_FILE)
     USER_NAME=$(jq -r ".USERS[$i].USER_NAME" $CONFIG_FILE)
 
     # Add a new cron task to the temporary cron file for each user
-    echo "${CRONTAB} /usr/local/bin/gtimeout 86400 /usr/bin/python3 geekbot.py $GUILD_ID \"$CHANNEL_NAME\" $USER_ID \"$USER_NAME\" $GEEKBOT_ID" >> /tmp/cron_temp
+    echo "${CRONTAB} /usr/local/bin/gtimeout 86400 /usr/bin/python3 \
+      ${SCRIPT_PATH}/geekbot.py $GUILD_ID \"$CHANNEL_NAME\" $USER_ID \"$USER_NAME\" \
+      $GEEKBOT_ID >> ~/geekbot/${TODAY}.log" >> /tmp/cron_temp
 done
 
 # Apply the updated cron file to crontab
