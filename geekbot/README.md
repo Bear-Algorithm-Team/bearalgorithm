4b1154799d2ab095981b450849a6ae6464a271d6
kangwooklee29 <141019638+kangwooklee29@users.noreply.github.com>
2023-08-11 18:39:30 +0900

    [PS-20] Improve geekbot to use log file (#6)
    Changelog
    ====
    - Logs the result of the cron job, to help debugging easily.
    - Fixes bugs of the previous code, including a typo in argument numbers,
    the config file, paths, package installation checking codes.
    - Adds descriptions for the bot.

--- geekbot/README.md
+++ geekbot/README.md
@@ -4,6 +4,22 @@
 
 The `setup_geekbot.sh` script allows you to automate daily report reminders on Discord. Once installed on your computer, it schedules a task to run every weekday at 10 AM. At this time, users listed in the config.json file will receive a Direct Message, prompting them to compose their daily report. These reminders are sent within the specified Discord channel from the configuration. Generally, this utility runs on an admin's machine, so there's no action required from the regular users.
 
+## How to set up
+
+### In an admin's machine
+
+```zsh
+./setup_geekbot.sh
+```
+
+Just run this command. Install other packages if it terminates with error messages about uninstalled packages.
+
+### In the server that should receive the daily report
+
+Just open this [link](https://discord.com/api/oauth2/authorize?client_id=1138890034393665698&permissions=2048&redirect_uri=https%3A%2F%2Flocalhost&response_type=code&scope=bot) and add this bot to the server. 
+
+Please note that the link is generated in Discord Developer Portal and can be changed if permissions are changed.
+
 ## Specification
 
 ### The content of `config.json`
@@ -12,8 +28,7 @@ The `setup_geekbot.sh` script allows you to automate daily report reminders on D
     "CRONTAB": "0 10 * * 1-5",
     "GUILD_ID": 1138900602823393320,
     "CHANNEL_NAME": "일반",
-    "USERS": [{"USER_ID": 882859405283041280}, {"USER_NAME": "Kangwook Lee"}],
-    "GEEKBOT_ID": "MTEzODg5MDAzNDM5MzY2NTY5OA.GfP9Zf.Lort5HAlrKrXuyyVjwGFYoxJ-ineIG3qoVoFuE"
+    "USERS": [{"USER_ID": 882859405283041280, "USER_NAME": "Kangwook Lee"}]
 }
 ```
 
@@ -28,5 +43,3 @@ The `setup_geekbot.sh` script allows you to automate daily report reminders on D
   - `USER_ID`: Each user can be identified either by their unique `USER_ID`. It can be obtained from Discord's developer mode.
   
   - `USER_NAME`: This is the name shown in the daily report and can be set as desired.
-
-- `GEEKBOT_ID`: The bot's unique authentication token, required for logging into Discord's API and performing actions on behalf of the bot user. You can obtain it when you create a new bot for Discord.
--- geekbot/setup_geekbot.sh
+++ geekbot/setup_geekbot.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# check the number of arguments
+if [ "$#" -eq 0 ] || [ "$#" -gt 1 ]; then
+    echo "Error: The number of arguments is not appropriate. You should use one argument for this command."
+    echo "Example: ./setup_geekbot.sh \"MDAzND....\" # The token of the discord bot"
+    exit 1
+fi
+
 # check if coreutils is installed
-if ! command -v coreutils &> /dev/null; then
+if ! command -v gls &> /dev/null; then
     echo "Error: coreutils is not installed."
     echo "Please install coreutils using 'brew install coreutils' and try again."
     exit 1
@@ -14,25 +21,44 @@ if ! command -v jq &> /dev/null; then
     exit 1
 fi
 
+# Check if the 'discord' module is installed in /usr/bin/python3
+if ! /usr/bin/python3 -c "import discord" 2>/dev/null; then
+    echo "Error: The 'discord' module is not installed for /usr/bin/python3."
+    echo "Please install it using the command: /usr/bin/pip3 install discord.py"
+    exit 1
+fi
+
 # Get values from config.json
-CONFIG_FILE="./config.json"
+SCRIPT_PATH="$(cd "$(dirname "$0")"; pwd -P)"
+CONFIG_FILE="${SCRIPT_PATH}/config.json"
 CRONTAB=$(jq -r '.CRONTAB' $CONFIG_FILE)
 GUILD_ID=$(jq -r '.GUILD_ID' $CONFIG_FILE)
 CHANNEL_NAME=$(jq -r '.CHANNEL_NAME' $CONFIG_FILE)
-GEEKBOT_ID=$(jq -r '.GEEKBOT_ID' $CONFIG_FILE)
 
-# Store the content of the current crontab in a temporary file
-crontab -l > /tmp/cron_temp
+# Initialize ~/.geekbot/run.sh
+mkdir -p ~/.geekbot
+RUN_FILE_PATH=~/.geekbot/run.sh
+rm -f $RUN_FILE_PATH
+echo "#!/bin/bash" >> $RUN_FILE_PATH
+chmod +x $RUN_FILE_PATH
 
 # Loop through each user in the USERS array
+GTIMEOUT_PATH=$(which gtimeout)
 length=$(jq '.USERS | length' $CONFIG_FILE)
 for (( i = 0 ; i < $length ; i++ )); do
     USER_ID=$(jq -r ".USERS[$i].USER_ID" $CONFIG_FILE)
     USER_NAME=$(jq -r ".USERS[$i].USER_NAME" $CONFIG_FILE)
 
-    # Add a new cron task to the temporary cron file for each user
-    echo "${CRONTAB} /usr/local/bin/gtimeout 86400 /usr/bin/python3 geekbot.py $GUILD_ID \"$CHANNEL_NAME\" $USER_ID \"$USER_NAME\" $GEEKBOT_ID" >> /tmp/cron_temp
+    echo "${GTIMEOUT_PATH} 86400 /usr/bin/python3 \
+      ${SCRIPT_PATH}/geekbot.py $GUILD_ID \"$CHANNEL_NAME\" $USER_ID \"$USER_NAME\" \
+      $1 >> ~/.geekbot/run.log" >> $RUN_FILE_PATH
 done
+
+# Store the content of the current crontab in a temporary file
+crontab -l > /tmp/cron_temp
+
+# Add a new cron task to the temporary cron file for each user
+echo "${CRONTAB} $RUN_FILE_PATH" >> /tmp/cron_temp
 
 # Apply the updated cron file to crontab
 crontab /tmp/cron_temp
