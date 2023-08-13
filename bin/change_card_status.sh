14020ba9f532cd06e9bec59c5724b53a2fe6d569
kangwooklee29 <141019638+kangwooklee29@users.noreply.github.com>
2023-08-13 19:13:43 +0900

    [PS-57] Introduce a new workflow that updates Trello when PR is updated (#20)
    Changelog
    ====
    - Introduces a new workflow that updates Trello when PR is updated. It
    reads the title of the updated PR and changes the status of the card
    which has the updated PR's ticket number into `Doing`.
    - Changes `change_card_to_done.sh` to `change_card_status.sh`.
    Previously it received only one argument but now it receives one more
    argument & uses it to specify the target list of the card.

--- bin/change_card_status.sh
+++ bin/change_card_status.sh
@@ -19,21 +19,23 @@ function find_card_id_of_ticket() {
 }
 
 function fetch_list_ids() {
+    local list_name="$1"
+
     # fetch all the lists
     response=$(curl --silent --request GET \
       --url "https://api.trello.com/1/boards/${BoardID}/lists?key=${APIKey}&token=${APIToken}" \
       --header 'Accept: application/json')
 
     # fetch the ID of the Done list
-    echo "$response" | jq -r '.[] | select(.name == "Done").id'
+    echo "$response" | jq -r --arg name "$list_name" '.[] | select(.name == $name).id'
 
     # fetch all the list IDs
     echo "$response" | jq -r '.[].id'
 }
 
 # Call function and capture output in an array
-IFS=$'\n' read -d '' -r -a output_array < <(fetch_list_ids && printf '\0')
-DoneListID="${output_array[0]}"
+IFS=$'\n' read -d '' -r -a output_array < <(fetch_list_ids "$2" && printf '\0')
+TargetListID="${output_array[0]}"
 list_ids=("${output_array[@]:1}")
 
 # find the card id of the ticket
@@ -44,7 +46,7 @@ for id in "${list_ids[@]}"; do
     curl --silent --request PUT \
       --url "https://api.trello.com/1/cards/${target_card_id}?key=${APIKey}&token=${APIToken}" \
       --header 'Accept: application/json' \
-      --data "idList=$DoneListID&pos=top" \
+      --data "idList=$TargetListID&pos=top" \
       -o /dev/null
     exit $?
   fi
