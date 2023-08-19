b8aed7eed8adc6d4c517eb3c919fc7ee67d70eed
sae1013 <sae1013@gmail.com>
2023-08-20 01:40:26 +0900

    solve

--- solutions/minwoo/baekjoon/2206 벽부수고 이동하기.py
+++ solutions/minwoo/baekjoon/2206 벽부수고 이동하기.py
@@ -2,14 +2,12 @@ from collections import deque
 
 n,m = map(int,input().split())
 grid = [input() for _ in range(n)]
-visit = [[[0]*m for _ in range(n)] for _ in range(2)]
 cache = [[[0]*m for _ in range(n)] for _ in range(2)]
 dy = [0,0,1,-1]
 dx = [1,-1,0,0]
 
 def bfs(i,j):
     q = deque()
-    visit[0][i][j] = 1
     cache[0][i][j] = 1
     q.append([0,i,j])
 
@@ -18,19 +16,18 @@ def bfs(i,j):
         for i in range(4):
             ny = y+dy[i]
             nx = x+dx[i]
+            if not(0<=ny<n and 0<=nx<m):
+                continue
             if z == 0 :  # 벽을 부술 수 있다
-                if 0<=ny<n and 0<=nx<m and visit[0][ny][nx] == 0 and grid[ny][nx] == "0":  
-                    visit[0][ny][nx] = 1
+                if  grid[ny][nx] == "0":  
                     cache[0][ny][nx] = cache[0][y][x]+1
                     q.append([0,ny,nx])
-                elif 0<=ny<n and 0<=nx<m and visit[1][ny][nx] == 0 and grid[ny][nx] == "1":
-                    visit[1][ny][nx] = 1
+                elif cache[1][ny][nx] == 0 and grid[ny][nx] == "1":
                     cache[1][ny][nx] = cache[0][y][x] + 1
                     q.append([1,ny,nx])
 
             elif z == 1 : #벽을 부술 수 없고, 같은층으로만 이동한다.
-                if 0<=ny<n and 0<=nx<m and visit[1][ny][nx] == 0 and grid[ny][nx] == "0" :
-                    visit[1][ny][nx] = 1
+                if cache[1][ny][nx] == 0 and grid[ny][nx] == "0" :
                     cache[1][ny][nx] = cache[1][y][x] + 1
                     q.append([1,ny,nx])
 
