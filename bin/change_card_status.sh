bcdd32483ba6148e0b39fb23aa0ce8bc47d68b4a
Kangwook Lee <kangwooklee29@gmail.com>
2023-09-07 02:58:11 +0900

    [PS-107] Add remove option for bearcard (#56)
    Changelog
    ====
    - Adds remove option for bearcard, to change the card status into
    "Deleted" using this command.
    - This option uses the script in `bin/change_card_status.sh`, already
    used by the workflow, so the lines of change is very few.
    - Adds the code to print the success/failure message for
    `bin/change_card_status.sh`, to show the result of the script clearly.
    - Refactors `bin/change_card_status.sh` & `bearcard` to change the API
    endpoint when fetching all the cards. Previously it fetched all the list
    IDs first and then it found the card ID using the list IDs, since I
    didn't know the API endpoint that can fetch all the cards even though I
    don't know the specific list ID. Now it directly fetches all the cards
    not using the list IDs.
    - Fixes a bug when running `bearcard --update-key` by introducing a
    function `check_config_file` in bearcard. Previously it didn't run
    properly since it needed the config file to run the code, due to my
    mistake.
    
    
    Detailed Description
    ====
    After this commit is merged, you can change the card status into
    "Deleted" by running `bearcard -r "Any string in the card name"` simply.

--- bin/change_card_status.sh
+++ bin/change_card_status.sh
@@ -3,16 +3,18 @@
 # Note: this script should run with the environmental variables such as APIKey, APIToken and BoardID.
 
 function find_card_id_of_ticket() {
-  local ListID=$1
-  local TicketNumber=$2
+  local TicketNumber=$1
 
   # fetch all the cards in a given list
   all_cards=$(curl --silent --request GET \
-  --url "https://api.trello.com/1/lists/${ListID}/cards?key=${APIKey}&token=${APIToken}" \
+  --url "https://api.trello.com/1/boards/${BoardID}/cards?key=${APIKey}&token=${APIToken}" \
   --header 'Accept: application/json')
 
+  # return full card name where contains TicketNumber
+  echo "$all_cards" | jq -r --arg ticket "$TicketNumber" '.[] | select(.name | contains($ticket)) | .name' | tr -d '\r'
+
   # return card id where name contains TicketNumber
-  echo "$all_cards" | jq -r --arg ticket "$TicketNumber" '.[] | select(.name | contains($ticket)) | .id'
+  echo "$all_cards" | jq -r --arg ticket "$TicketNumber" '.[] | select(.name | contains($ticket)) | .id' | tr -d '\r'
 }
 
 function fetch_list_ids() {
@@ -25,28 +27,35 @@ function fetch_list_ids() {
 
     # fetch the ID of the Done list
     echo "$response" | jq -r --arg name "$list_name" '.[] | select(.name == $name).id'
-
-    # fetch all the list IDs
-    echo "$response" | jq -r '.[].id'
 }
 
-# Call function and capture output in an array
-IFS=$'\n' read -d '' -r -a output_array < <(fetch_list_ids "$2" && printf '\0')
-TargetListID="${output_array[0]}"
-list_ids=("${output_array[@]:1}")
+# fetch the target list id
+TargetListID=$(fetch_list_ids "$2")
 
 # find the card id of the ticket
-for id in "${list_ids[@]}"; do
-  target_card_id=$(find_card_id_of_ticket "$id" "$1")
+  IFS=$'\n' read -d '' -r -a output_array < <(find_card_id_of_ticket "$1" && printf '\0')
+  target_card_name="${output_array[0]}"
+  target_card_id="${output_array[1]}"
   if [ -n "$target_card_id" ]; then
+    ERROR_FILE=$(mktemp)
+
     # Update Trello
     curl --silent --request PUT \
       --url "https://api.trello.com/1/cards/${target_card_id}?key=${APIKey}&token=${APIToken}" \
       --header 'Accept: application/json' \
       --data "idList=$TargetListID&pos=top" \
-      -o /dev/null
+      -o /dev/null 2> "$ERROR_FILE"
+
+    if [ $? -eq 0 ]; then
+      echo "Successfully changed the status of the card \"$target_card_name\" into \"$2\"."
+    else
+      echo "Failed to change the status of the card \"$target_card_name\" into \"$2\": "
+      cat "$ERROR_FILE"
+    fi
+
+    rm "$ERROR_FILE"
     exit $?
   fi
-done
 
+echo "Failed to find the card containing the given string \"$1\""
 exit 1
