1512b640d3061b83ccb4df55164fb5d98741066c
Kangwook Lee <kangwooklee29@gmail.com>
2023-09-21 07:05:49 +0900

    Refactor Minwoo's code for this problem

--- solutions/kangwook/programmers 연습문제/광물캐기.py
+++ solutions/kangwook/programmers 연습문제/광물캐기.py
@@ -1,29 +1,46 @@
 def solution(picks, minerals):
-    """
-    
-    dp[(dia, iron, stone)] = 곡괭이를 (dia, iron, stone)개 사용했을 때 최소 피로도
-    
-    """
-    fatigue_table = [{"diamond":1, "iron":1, "stone": 1}, 
-                     {"diamond":5, "iron":1, "stone": 1}, 
-                     {"diamond":25, "iron":5, "stone": 1}]
-    dp = {}
-    picks = tuple(picks)
-    def calc_fatigue(minerals, cur_pick):
-        return sum(fatigue_table[cur_pick][mineral] for mineral in minerals)
-    
-    def dfs(minerals, cur_picks):
-        if cur_picks in dp:
-            return dp[cur_picks]
-        if cur_picks > picks: 
-            return float('inf')
-        if sum(cur_picks) == sum(picks): 
-            return 0
+    material_map = {
+        'diamond': {
+            'diamond':1,
+            'iron':1,
+            'stone':1
+        },
+        'iron':{
+            'diamond':5,
+            'iron':1,
+            'stone':1
+        },
+        'stone': {
+            'diamond':25,
+            'iron':5,
+            'stone':1
+        }
+    }
+    pick_map = {
+        0:'diamond',
+        1:'iron',
+        2:'stone'
+    }
 
-        j, k, l = cur_picks
-        dp[cur_picks] = min(dfs(minerals[5:], (j + 1, k, l)) + calc_fatigue(minerals[: 5], 0), \
-                            dfs(minerals[5:], (j, k + 1, l)) + calc_fatigue(minerals[: 5], 1), \
-                            dfs(minerals[5:], (j, k, l + 1)) + calc_fatigue(minerals[: 5], 2))
-        
-        return dp[cur_picks]
-    return dfs(minerals, (0, 0, 0))
+    def calc_effort(pick, mineral_group):
+        return sum(material_map[pick][mineral] for mineral in mineral_group)
+
+    listed_picks = [] # 작은 피로도를 사용하는 곡괭이부터, 사용가능한 개수만큼 이를 나열한 리스트
+    for i, pick in enumerate(picks): 
+        listed_picks += [pick_map[i] for _ in range(pick)]
+
+    mineral_groups = [] # 광물들을 5개 크기 버켓으로 잘라서 저장한 리스트
+    for i in range(0, len(minerals), 5):
+        mineral_groups += [minerals[i:i+5]]
+        if len(mineral_groups) == len(listed_picks):
+            break
+
+    # 돌 곡괭이를 사용하는 경우 높은 피로도가 드는 버켓부터 내림차순 정렬. 이렇게 하면, 높은 피로도가 드는 버켓부터 작은 피로도를 사용하는 곡괭이를 사용하게 하면 된다.
+    mineral_groups.sort(key = lambda x: -calc_effort('stone', x)) 
+
+    answer = 0
+    for pick, mineral_group in zip(listed_picks[:len(mineral_groups)], mineral_groups):
+        answer += calc_effort(pick, mineral_group)
+    return answer
+
+print(solution([1, 3, 2], ["diamond", "diamond", "diamond", "iron", "iron", "diamond", "iron", "stone"]))
