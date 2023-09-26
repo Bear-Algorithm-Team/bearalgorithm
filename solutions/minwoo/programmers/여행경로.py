5c8c6a35890e2744c3267299016f1813099fb8a1
MJ <sae1013@gmail.com>
2023-09-26 21:48:23 +0900

    [PS-118] 여행경로 (#65)
    Link
    ====
    https://school.programmers.co.kr/learn/courses/30/lessons/43164
    
    Approach
    ====  
    DFS 백트래킹 
    평소에 DFS를 풀때, 글로벌 변수들을 많이 활용했는데 강욱님 코드들, 재귀 잘짜는 사람들 보니까 재귀함수의 리턴값을 결과로
    받아서 처리하는 코드들이 훨씬 좋아보였음.
    
    그동안 글로벌 변수를 활용한 이유는 기저조건을 잘 설정하고, 원하는 깊이에 도달했을 때 답을 저장 해주기만하면 코드 짜기가 매우
    쉬워지기때문.
    그러나 글로벌변수 활용은 실수/디버깅 시 좋지않음. 또한 답을 출력할때 불필요한 코드들이 들어간다.
    재귀문제푸는 습관을 고치는게 좋을것같다
    
    Time Complexity
    ====  
    O(N)
    
    Space Complexity (optional)
    ====  
    O(N) : 재귀깊이
    
    Detailed Description
    ====
    
    ---------
    
    Co-authored-by: Kangwook Lee <kangwooklee29@gmail.com>

---
+++ solutions/minwoo/programmers/여행경로.py
@@ -0,0 +1,21 @@
+
+def solution(tickets):
+    def dfs(node,path): 
+
+        for i in range(len(tickets)):
+            a,b = tickets[i] # a -> b 
+            if visit[i] == 0 and a == node : # 다음 방문노드를 살핀다.
+                visit[i] = 1
+                path.append(b) # 다음방문노드
+                res = dfs(b,path) 
+                if len(res) == len(tickets)+1: # 모든 티켓을 사용하면, path를 바로 리턴한다.
+                    return path
+                path.pop()
+                visit[i] = 0
+        return path
+        
+    tickets.sort(key = lambda x : x[1])
+    visit = [0]*(len(tickets))
+    
+    return dfs("ICN",["ICN"],0) 
+
