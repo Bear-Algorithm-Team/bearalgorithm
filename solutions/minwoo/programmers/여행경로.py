c518e4b3c5bf3df2ff33f16eeadf52949336a832
MJ <sae1013@gmail.com>
2023-09-26 21:47:46 +0900

    Update solutions/minwoo/programmers/여행경로.py
    Co-authored-by: Kangwook Lee <kangwooklee29@gmail.com>

--- solutions/minwoo/programmers/여행경로.py
+++ solutions/minwoo/programmers/여행경로.py
@@ -7,7 +7,7 @@ def solution(tickets):
             if visit[i] == 0 and a == node : # 다음 방문노드를 살핀다.
                 visit[i] = 1
                 path.append(b) # 다음방문노드
-                res = dfs(b,path,ticket_cnt+1) 
+                res = dfs(b,path) 
                 if len(res) == len(tickets)+1: # 모든 티켓을 사용하면, path를 바로 리턴한다.
                     return path
                 path.pop()
