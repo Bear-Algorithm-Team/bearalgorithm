4743706ea40daf4b2b1b924abb1d682d8a4d3908
Kangwook Lee <kangwooklee29@gmail.com>
2023-10-21 23:59:55 +0900

    [PS-127] 프로그래머스 아이템 줍기 (#73)
    Link
    ====
    https://school.programmers.co.kr/learn/courses/30/lessons/87694
    
    Approach
    ====  
    BFS..?
    
    Time Complexity
    ==== 
    O(n^2) 
    
    Space Complexity (optional)
    ====  
    
    Detailed Description
    ====  
    - 굳이 따지자면 bfs 문제긴 한데, bfs 관련 구현보다는 '이 점은 지금 사각형 안인가 밖인가' '이 점은 테두리 위에 있는
    점인가 아닌가' 판단을 효율적으로 구현하는 걸 잘하는 게 더 중요한 문제.
    - 처음에는 점이 사각형 안에 있는지, 테두리 위에 있는지 여부를 판단하는 걸 어떻게 구현할지 한참 고민하다가 2차원 grid
    배열을 만들고 그 안에 1을 채우는 식으로 접근해 풀긴 했는데, 다른 사람들이 푼 걸 보니 그냥 부등식으로 확인하는 부분만
    깔끔하게 구현하면 전혀 어렵게 생각할 게 없는 문제였음... 괜히 혼자 조급해서 크게 못 보고 너무 어렵게 생각하는 습관이 잘 안
    고쳐지는 거 같다. 좀 더 여유있게 생각할 수 있으면 좋겠는데..

---
+++ solutions/kangwook/programmers 연습문제/아이템줍기.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def solution(rectangle, characterX, characterY, itemX, itemY):
+    def in_square(cur_x, cur_y, nx, ny):
+        mid_x, mid_y = (cur_x + nx) / 2, (cur_y + ny) / 2
+        return any(x1 < mid_x < x2 and y1 < mid_y < y2 for x1, y1, x2, y2 in rectangle)
+    
+    def on_border(cur_x, cur_y, nx, ny):
+        mid_x, mid_y = (cur_x + nx) / 2, (cur_y + ny) / 2
+        return any((x1 < mid_x < x2 and (y1 == mid_y or y2 == mid_y)) 
+                   or (y1 < mid_y < y2 and (x1 == mid_x or x2 == mid_x)) for x1, y1, x2, y2 in rectangle)
+
+    q = deque([(characterX, characterY, 0)])
+    visited = set()
+    while q:
+        cur_x, cur_y, cnt = q.popleft()
+        if (cur_x, cur_y) == (itemX, itemY): return cnt
+        for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            nx, ny = cur_x + dx, cur_y + dy
+            if (nx, ny) in visited \
+                or in_square(cur_x, cur_y, nx, ny) or not on_border(cur_x, cur_y, nx, ny): continue
+            visited.add((nx, ny))
+            q.append((nx, ny, cnt + 1))
